abe27e1040180183f752fb4951b0fcff5a7906e0
Rui Xu <rxu@officeworks.com.au>
2020-05-27 19:09:22 +1000

    sum of multi

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+        => Enumerable.Range(1, max - 1)
+            .Where(num => multiples.Any(e => e != 0 && num % e == 0))
+            .Sum();
+}
