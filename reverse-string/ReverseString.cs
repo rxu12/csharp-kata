6499db85aec1ba0c2991b978cd2f673121919649
Rui Xu <rxuofficeworks@gmail.com>
2020-05-30 22:23:36 +1000

    reverse string

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string reversed = "";
+        foreach (char c in input)
+        {
+            reversed = c + reversed;
+        }
+        return reversed;
+    }
+}
