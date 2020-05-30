3194addd62a67933e2b630ee114047734e1bb54e
Rui Xu <rxuofficeworks@gmail.com>
2020-05-30 23:13:49 +1000

    armstrong num

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        int source = number;
+        List<int> digits = new List<int>();
+        while (source  > 0)
+        {
+            int digit = source % 10;
+            source /= 10;
+            digits.Add(digit);
+        }
+        return digits.Aggregate(0,
+            (acc, digit) => (int)(acc + Math.Pow(digit, digits.Count))) == number;
+    }
+}
