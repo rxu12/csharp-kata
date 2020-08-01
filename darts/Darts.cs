300e598ea7e265d24fda384289dc3c741700d17b
Rui Xu <rxu@192-168-1-106.tpgi.com.au>
2020-08-01 20:29:56 +1000

    Darts

---
+++ darts/Darts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        var range = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        return range > 10 ? 0 : range > 5 ? 1 : range > 1 ? 5 : 10;
+    }
+}
