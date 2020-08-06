4da74a07bc37549b3ef247d429989f8acfa8cf64
Rui Xu <rxuofficeworks@gmail.com>
2020-08-06 17:16:25 +1000

    Proverb

---
+++ proverb/Proverb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        return subjects
+        .Select((wd, i) => i != subjects.Length - 1
+            ? $"For want of a {wd} the {subjects[i + 1]} was lost."
+            : $"And all for the want of a {subjects[0]}.").ToArray();
+    }
+}
